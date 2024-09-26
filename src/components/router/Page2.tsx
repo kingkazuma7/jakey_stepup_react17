85826d08a11534d54e02be5f785c7f297416cb12
kingkazuma7 <your.nasionasio7@gmail.com>
2024-09-27 08:51:56 +0900

    24. クエリパラメーター - useLocation - ?以降の文字が入る - UrlSearchParamsの引数としてわたす - query.get('name')メソッドでvalueを取得

--- src/components/router/Page2.tsx
+++ src/components/router/Page2.tsx
@@ -6,6 +6,8 @@ export const Page2 = () => {
       <h1>Page2です</h1>
       <Link to="/page2/1000">UrlParameter - :idで動的に表示</Link>
       <br />
+      <Link to="/page2/1000?name=tanaka&age=18">Query Parameter</Link>
+      <br />
       <Link to="/page2/detailA">DetailA</Link>
     </>
   );
--- src/components/router/UrlParameter.tsx
+++ src/components/router/UrlParameter.tsx
@@ -1,11 +1,17 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 export const UrlParameter = () => {
   const { id } = useParams() as { id: string };
+  const { search } = useLocation(); // ?name=tanaka&age=18
+  const query = new URLSearchParams(search);
+
   return (
     <>
       <h1>UrlParameterページです。</h1>
       <p>:idで渡されたurlパラメーターは {id} です</p>
+      <p>
+        クエリパラメーターは {query.get('name')}と{query.get('age')}歳です。
+      </p>
     </>
   );
 };
