c63d1aa00c91184c60981639126eb62dd4f3a88f
kingkazuma7 <your.nasionasio7@gmail.com>
2024-09-27 10:09:55 +0900

    25 stateを渡す
    ※一覧から詳細に渡すときなど

--- src/components/router/Page1DetailA.tsx
+++ src/components/router/Page1DetailA.tsx
@@ -1,3 +1,7 @@
+import { useLocation } from 'react-router-dom';
+
 export const Page1DetailA = () => {
+  const { state, key } = useLocation();
+  console.log(state, key);
   return <div>Page1DetailAのページです。</div>;
 };
